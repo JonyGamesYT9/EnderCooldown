48b66540ea1378789954372141816ec8c53aacd0
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-08-15 09:10:44 -0500

    update EnderCooldown.php

--- src/JonyGamesYT9/EnderCooldown/EnderCooldown.php
+++ src/JonyGamesYT9/EnderCooldown/EnderCooldown.php
@@ -1,1 +1,82 @@
+<?php
 
+namespace JonyGamesYT9\EnderCooldown;
+
+use pocketmine\Player;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\item\EnderPearl;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use function time;
+use function str_replace;
+
+/**
+* Class EnderCooldown
+* @package JonyGamesYT9\EnderCooldown
+*/
+class EnderCooldown extends PluginBase implements Listener
+{
+
+  /** @var Config $config */
+  private $config;
+
+  /** @var array $cooldowns */
+  private $cooldowns = [];
+
+  /**
+  * @return void
+  */
+  public function onEnable(): void
+  {
+    $this->saveResource("config.yml");
+    $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+    if ($this->config->get("version") === 1) {
+      if ($this->config->get("cooldown-time") == 0 || !is_numeric($this->config->get("cooldown-time"))) {
+        $this->getLogger()->error("EnderCooldown: The cooldown is not numeric, change it and restart the server.");
+        $this->getServer()->getPluginManager()->disablePlugin($this);
+      } else {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      }
+    } else {
+      $this->getLogger()->error("EnderCooldown: Error in config.yml please delete file and restart server!");
+      $this->getServer()->getPluginManager()->disablePlugin($this);
+    }
+  }
+
+  /**
+  * @param PlayerQuitEvent $event
+  * @return void
+  */
+  public function onQuit(PlayerQuitEvent $event): void
+  {
+    $player = $event->getPlayer();
+    if ($player instanceof Player) {
+      if (isset($this->cooldowns[$player->getName()])) {
+        unset($this->cooldowns[$player->getName()]);
+      }
+    }
+  }
+
+  /**
+  * @param PlayerInteractEvent $event
+  * @return void
+  */
+  public function onUseEnder(PlayerInteractEvent $event): void
+  {
+    $item = $event->getItem();
+    $player = $event->getPlayer();
+    if ($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_AIR) {
+      if ($item instanceof EnderPearl) {
+        if (isset($this->cooldown[$player->getName()])) {
+          $event->setCancelled(true);
+          $time = $this->config->get("cooldown-time") - (time() - $this->cooldowns[$player->getName()]);
+          $player->sendMessage(str_replace(["&", "{cooldown}"], ["§", $time], $this->config->get("message-hascooldown")));
+        } else {
+          $this->cooldowns[$player->getName()] = (time() + $this->config->get("cooldown-time"));
+        }
+      }
+    }
+  }
+}
